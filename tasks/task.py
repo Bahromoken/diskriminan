94db920263b133ce44df86f7f60ed17cfb32bf82
Дмитрий Колб <dm.kolb@gmail.com>
2022-10-25 16:36:20 +0000

    Update tasks/task.py

--- tasks/task.py
+++ tasks/task.py
@@ -3,8 +3,8 @@ from math import sqrt
 
 
 def solution(a, b, c) -> Optional[Tuple[float, float]]:
-
+    # Add your code here or call it from here  
     result = tuple()
-    # Add your code here or call it from here   
+ 
 
     return result 
