104f288883cc51967bc29664690c0be7ecf43910
Дмитрий Колб <dm.kolb@gmail.com>
2022-10-26 09:12:00 +0000

    Update tasks/task.py

--- tasks/task.py
+++ tasks/task.py
@@ -1,8 +1,8 @@
-from typing import Tuple, Optional
+from typing import Tuple, Union
 from math import sqrt
 
 
-def solution(a, b, c) -> Optional[Tuple[float, float]]:
+def solution(a, b, c) -> Union[Tuple[float, float], Tuple[float], None]:
     # Add your code here or call it from here  
     result = tuple()
  
