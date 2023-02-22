e05a8e0a89ae0efb0b72bf3ac288afe0d04bcc54
Muhammad <magdiyevbahrom@gmail.com>
2023-02-22 22:22:15 +0500

    please work

--- tasks/task.py
+++ tasks/task.py
@@ -3,12 +3,12 @@ from math import sqrt
 
 
 def solution(a, b, c) -> Union[Tuple[float, float], Tuple[float], None]:
-    diskriminant = (b * b) - (4 * a * c)
-    if diskriminant > 0:
-        result = (-b - sqrt(diskriminant)) / 2 * a, (-b + sqrt(diskriminant)) / 2 * a
-    if diskriminant == 0:
-        result = (-b) / 2 * a
-
-    result = tuple()
-
+    result = None
+    discriminant = b * b - 4 * a * c
+    if discriminant > 0:
+        result = (-b - sqrt(discriminant)) / (2 * a), (-b + sqrt(discriminant)) / (2 * a)
+    if discriminant == 0:
+        result = -b / 2 * a
     return result
+
+
